212ddb4a3e43d45910295b35f278104a24813aaf
Dodo <dodo55.dev@gmail.com>
2013-04-21 13:53:40 +0200

    Fixed previous change causing multiple inclusions resulting in fatal class redefinitions

--- lib/db/db.php
+++ lib/db/db.php
@@ -1,7 +1,8 @@
 <?php
 namespace APDL;
-require(__DIR__."/db_connection.php");
-require(__DIR__."/db_mysqli.php");
-require(__DIR__."/db_record.php");
-require(__DIR__."/db_utils.php");
+require_once(__DIR__ . "/db_connection.php");
+require_once(__DIR__ . "/db_mysqli.php");
+require_once(__DIR__ . "/db_record.php");
+require_once(__DIR__ . "/db_utils.php");
+
 log("Database layer loaded", Log::L_INFO);
